ab9b5fd60fb399297f39dfd2ab5e95840761e4a1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-24 14:31:30 +0000

    24 Jun 2023, 20:01

---
+++ src/use-cases/claim-reservation-use-case.ts
@@ -0,0 +1,31 @@
+// src/usecases/ClaimReservationUseCase.ts
+import { ReservationRepository } from '../interfaces';
+
+export interface ClaimReservationUseCase {
+  execute(machineId: number, pin: number): boolean;
+}
+
+export class ClaimReservationInteractor implements ClaimReservationUseCase {
+
+  constructor(private reservationRepository: ReservationRepository) {}
+
+  execute(machineId: number, pin: number): boolean {
+    
+    const reservation = this.reservationRepository
+      .getAll()
+      .find((r) => r.machineNumber === machineId && r.pin === pin);
+
+    if (reservation) {
+      if (reservation.used) {
+        return false; // Reservation already claimed
+      }
+
+      reservation.used = true;
+      this.reservationRepository.update(reservation);
+
+      return true;
+    }
+
+    return false;
+  }
+}
