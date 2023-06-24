ce1e7ec8dea271cbd76fcb60160fdfd7317a3955
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-24 08:57:10 +0000

    24 Jun 2023, 14:27

--- src/Reservation.ts
+++ src/Reservation.ts
@@ -1,20 +1,23 @@
 // Reservation.ts
 export class Reservation {
-  id: string;
-  machineNumber: number;
-  reservationDateTime: Date;
-  pin: number;
-  used: boolean;
-  failedAttempts: number;
-  phoneNumber: string;
 
-  constructor(id: string, machineNumber: number, reservationDateTime: Date, pin: number, phoneNumber: string) {
-    this.id = id;
-    this.machineNumber = machineNumber;
-    this.reservationDateTime = reservationDateTime;
-    this.pin = pin;
-    this.used = false;
-    this.failedAttempts = 0;
-    this.phoneNumber = phoneNumber;
+  private used: boolean=false;
+  private failedAttempts: number=0;
+
+  constructor(
+    private readonly id: string, 
+    private readonly machineNumber: number, 
+    private reservationDateTime: Date, 
+    private pin: number,
+    private phoneNumber: string) {}
+
+  getFailedAttempts(){
+    this.failedAttempts
   }
+
+  markAsFiledAttempt(){
+    this.failedAttempts++
+  }
+
+
 }
