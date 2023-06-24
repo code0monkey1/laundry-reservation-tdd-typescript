a0a2c48abd165771565580d45967cf37979ac9cb
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-24 08:38:30 +0000

    24 Jun 2023, 14:08

---
+++ test/reservation-api.test.ts
@@ -0,0 +1,19 @@
+describe('reservation-api', () => {
+  
+})
+
+
+interface IReservation{
+     reserve(machineId:number,pin:number):void
+}
+function getReservationApi(){
+    
+    return {
+
+        reserve(machineId:number,pin:number){
+            
+        }
+    }
+
+
+}
