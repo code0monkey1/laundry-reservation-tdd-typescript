53e3ca8c94222aeefd9f9979b45b4c5ced41b7f3
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-24 05:25:14 +0000

    24 Jun 2023, 10:55

---
+++ src/MachineServiceImpl.ts
@@ -0,0 +1,13 @@
+class MachineService implements IMachineService{
+   
+  constructor(private machines:Array<number>){}
+
+  lock(reservationId: string, machineNumber: number, reservationDateTime: Date, pin: number): boolean {
+    throw new Error("Method not implemented.");
+  }
+  unlock(reservationId: string): void {
+    throw new Error("Method not implemented.");
+  }
+
+  
+}
