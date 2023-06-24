d37ea48c7fdbb720655ad802d4703502eca3eb59
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-24 14:23:31 +0000

    24 Jun 2023, 19:53

---
+++ src/use-cases/lock-machine-use-case.ts
@@ -0,0 +1,27 @@
+import { MachineDevice, ReservationRepository } from '../interfaces';
+
+export interface LockMachineUseCase {
+  execute(machineNumber: number, reservationId: string): boolean;
+}
+
+export class LockMachineInteractor implements LockMachineUseCase {
+  constructor(
+    private machineDevice: MachineDevice,
+    private reservationRepository: ReservationRepository
+  ) {}
+
+  execute(machineNumber: number, reservationId: string): boolean {
+    const reservation = this.reservationRepository.getById(reservationId);
+    if (reservation && reservation.machineNumber === machineNumber) {
+      const locked = this.machineDevice.lock(
+        reservationId,
+        reservation.reservationDateTime,
+        reservation.pin
+      );
+      if (locked) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
