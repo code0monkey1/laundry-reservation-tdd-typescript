83d9b426c09cf5d4cd19cc766ac126da685b9d98
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-24 07:09:26 +0000

    24 Jun 2023, 12:39

--- test/machine-api.test.ts
+++ test/machine-api.test.ts
@@ -1,3 +1,38 @@
+
 describe('machine-api', () => {
-  
+     
+     describe('lock',()=>{
+            
+           it('is defined',()=>{
+             
+             //Arrange
+             const sut = new MachineApi()
+
+             //Act //Assert
+             expect(sut.lock).toBeDefined()
+
+            
+
+           })
+
+             
+     })
 })
+
+interface IMachineAPI{ 
+
+     lock(reservationId:string ,machineNumber:number,reservationDateTime:Date,pin:number):boolean
+     unlock( reservationId:string):void
+
+} 
+
+class MachineApi implements IMachineAPI{
+  lock(reservationId: string, machineNumber: number, reservationDateTime: Date, pin: number): boolean {
+    throw new Error("Method not implemented.")
+  }
+  unlock(reservationId: string): void {
+    throw new Error("Method not implemented.")
+  }
+  
+}
+
