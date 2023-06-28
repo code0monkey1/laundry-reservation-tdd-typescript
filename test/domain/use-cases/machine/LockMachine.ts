d1423c2d23089337bb43fbec8be7bff315358549
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-28 13:44:35 +0000

    28 Jun 2023, 19:14

--- test/domain/use-cases/machine/LockMachine.ts
+++ test/domain/use-cases/machine/LockMachine.ts
@@ -1,9 +1,10 @@
-import { LockMachineUseCase } from "../../../../src/domain/interfaces/use-cases/machine/lock-machine-use-case";
+// import { LockMachineUseCase, LockRequest } from "../../../../src/domain/interfaces/use-cases/machine/lock-machine-use-case";
 
-class LockMachine implements LockMachineUseCase{
-  
-  execute(reservationId: string, machineNumber: string, reservationDateTime: string): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
+// class LockMachine implements LockMachineUseCase{
+ 
+//   execute(lockRequest: LockRequest): Promise<boolean> {
+//     throw new Error("Method not implemented.");
+//   }
 
-}
+
+// }
