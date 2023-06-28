33348c64c38c55222d4bea9fd879368ba7c3651f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-28 03:48:02 +0000

    28 Jun 2023, 09:18

--- test/domain/use-cases/reservation/make-reservation-use-case.test.ts
+++ test/domain/use-cases/reservation/make-reservation-use-case.test.ts
@@ -3,17 +3,21 @@ describe('Make Reservation Use Case',()=>{
      
   describe('Make Reservation', () => {
     
-    it.todo('should make reservation',()=>{
-  
+    describe('Should Execute All Reservation Functions',()=>{
+         
+         it.todo('should send reservation email',()=>{
+             
           const makeReservation = new MakeReservation()
            
           const reservationDateTime='01/01/2020, 01:01:12'
           const phoneNumber='123'
           const email='e@email.com'
           
-         const result= makeReservation.execute(reservationDateTime,phoneNumber,email)
+          makeReservation.execute(reservationDateTime,phoneNumber,email)
+
+         })
+       
   
-         expect(result).toBeTruthy()
    
         })
 
