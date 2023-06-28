871cbf1ebac91a176fe9e39523a2d6bfe1bb9e88
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-28 02:31:25 +0000

    28 Jun 2023, 08:01

--- test/CreateReservation.test.ts
+++ test/CreateReservation.test.ts
@@ -1,103 +1,108 @@
 import CreateReservation, { EmailService, MachineApi, ReservationRepository } from "../src/CreateReservation";
 import { Reservation } from "../src/types/Reservation";
 
-describe('CreateReservation', () => {
-
-     class MockEmailService implements EmailService{
-      
-       private mailSent:{machineNumber:number,reservationId:string,pin:string}[]=[]
-       
-       send(machineNumber: number, reservationId: string, pin: string): void {
-       
-        this.mailSent.push( {
-            machineNumber,
-            reservationId,
-            pin
-          })
-          
-       }
-
-       getSentMail(){
-        return this.mailSent
-       }
-      
-     }
-
-     class MockMachineApi implements MachineApi{
-       lock(reservationId: string, machineNumber: string, reservationDateTime: Date): boolean {
-         throw new Error("Method not implemented.");
-       }
-       unlock(machineNumber: string, reservationId: string): void {
-         throw new Error("Method not implemented.");
-       }
-      
-     }
-
-     class MockReservationRepository implements ReservationRepository{
-       getFreeMachineNumber(): number {
-         return 1
-       }
-       save(reservation: Reservation): void {
-         throw new Error("Method not implemented.");
-       }
-      
-     }
-
-
-     let mockEmailService:MockEmailService
-     let mockMachineApi:MachineApi
-     let mockReservationRepository:ReservationRepository
-
-     beforeEach( ()=>{
-      
-        jest.clearAllMocks()
-        mockEmailService = new MockEmailService()
-        mockMachineApi= new MockMachineApi()
-        mockReservationRepository=new MockReservationRepository()
-      
-     })
-    
-  test('sends email',()=>{
-
-     //Arrange
-       const createReservation = new CreateReservation(
-                                                  mockEmailService,
-                                                  mockReservationRepository,
-                                                  mockMachineApi)
-       
-     
-       const reservationDateTime = new Date('01/01/2020') 
-       const phoneNumber='1'  
-       const email="mail@gmail.com"                                        
-
-     //Act
-       
-       createReservation.execute(reservationDateTime,phoneNumber,email)
-        
-       const sentMail = mockEmailService.getSentMail()
-       expect(sentMail).toStrictEqual([{machineNumber:1,pin:"12345",reservationId:"2",}])
-
-     //Assert
-
-  })
-
-    it.todo('saves reservation to db')
-    it.todo('sends lock instructions to selected machine')
-
-    
-})
-
-
-describe('Machine Api', () => {
-     
-    it.todo('locks machine using machine number, DateTime of reservation and reservationId')
-
-    it.todo('unlocks machine using machineNumber and reservationId')
-})
-
-
-describe('ClaimReservation', () => {
+describe('Laundry Reservation', () => {
   
-})
+  describe('CreateReservation', () => {
+  
+       class MockEmailService implements EmailService{
+        
+         private mailSent:{machineNumber:number,reservationId:string,pin:string}[]=[]
+         
+         send(machineNumber: number, reservationId: string, pin: string): void {
+         
+          this.mailSent.push( {
+              machineNumber,
+              reservationId,
+              pin
+            })
+            
+         }
+  
+         getSentMail(){
+          return this.mailSent
+         }
+        
+       }
+  
+       class MockMachineApi implements MachineApi{
+         lock(reservationId: string, machineNumber: string, reservationDateTime: Date): boolean {
+           throw new Error("Method not implemented.");
+         }
+         unlock(machineNumber: string, reservationId: string): void {
+           throw new Error("Method not implemented.");
+         }
+        
+       }
+  
+       class MockReservationRepository implements ReservationRepository{
+         getFreeMachineNumber(): number {
+           return 1
+         }
+         save(reservation: Reservation): void {
+           throw new Error("Method not implemented.");
+         }
+        
+       }
+  
+  
+       let mockEmailService:MockEmailService
+       let mockMachineApi:MachineApi
+       let mockReservationRepository:ReservationRepository
+  
+       beforeEach( ()=>{
+        
+          jest.clearAllMocks()
+          mockEmailService = new MockEmailService()
+          mockMachineApi= new MockMachineApi()
+          mockReservationRepository=new MockReservationRepository()
+        
+       })
+      
+    test('sends email',()=>{
+  
+       //Arrange
+         const createReservation = new CreateReservation(
+                                                    mockEmailService,
+                                                    mockReservationRepository,
+                                                    mockMachineApi)
+         
+       
+         const reservationDateTime = new Date('01/01/2020') 
+         const phoneNumber='1'  
+         const email="mail@gmail.com"                                        
+  
+       //Act
+         
+         createReservation.execute(reservationDateTime,phoneNumber,email)
+          
+         const sentMail = mockEmailService.getSentMail()
+         expect(sentMail).toStrictEqual([{machineNumber:1,pin:"12345",reservationId:"2",}])
+  
+       //Assert
+  
+    })
+  
+      it.todo('saves reservation to db')
+      it.todo('sends lock instructions to selected machine')
+  
+      
+  })
+  
+  
+  describe('Machine Api', () => {
+       
+      it.todo('locks machine using machine number, DateTime of reservation and reservationId')
+  
+      it.todo('unlocks machine using machineNumber and reservationId')
+  })
+  
+  
+  describe('ClaimReservation', () => {
+    
+  })
+  
+
+})  
 
 
