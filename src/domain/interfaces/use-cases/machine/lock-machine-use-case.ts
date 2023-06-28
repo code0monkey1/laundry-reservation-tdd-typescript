c19d96ba09326ee0f72022f209d122a52e371c29
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-28 13:23:22 +0000

    28 Jun 2023, 18:53

--- src/domain/interfaces/use-cases/machine/lock-machine-use-case.ts
+++ src/domain/interfaces/use-cases/machine/lock-machine-use-case.ts
@@ -1,6 +1,28 @@
 interface LockMachineUseCase {
-    
+         
+  execute(reservationId:string, machineNumber:string, reservationDateTime:string):void
+        
 
 }
 
 
+export interface MachineApi{
+   
+  lock(lockRequest:LockRequest):Promise<boolean>
+
+  unlock():void
+
+}
+
+
+export type LockRequest={
+  reservationId:string,
+  machineNumber:string,
+  reservedDateTime:string
+}
+
+export interface MachineDevice{
+
+    lock(reservationId:string,  reservationDateTime:string,  pin:string):boolean
+    unlock( reservationId:string):void
+}
