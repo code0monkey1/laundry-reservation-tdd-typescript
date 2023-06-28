9ae71fd32387c87d6680b823a9e1fc7b4d77e930
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-28 14:15:55 +0000

    28 Jun 2023, 19:45

---
+++ src/domain/use-cases/machine/LockMachine.ts
@@ -0,0 +1,23 @@
+import { ReservationRepository } from "../../interfaces/repositories/reservation-repository";
+import { LockMachineUseCase, LockRequest } from "../../interfaces/use-cases/machine/lock-machine-use-case";
+
+export class LockMachine implements LockMachineUseCase{
+      
+  constructor(private readonly reservationRepository:ReservationRepository){}
+    
+
+  execute(lockRequest: LockRequest): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+
+  
+}
+
+
+export interface MachineDevice{
+
+    lock(reservationId:string,  reservationDateTime:string,  pin:string):boolean
+
+    unlock( reservationId:string):void
+
+}
