69e10c4d5bf3b137bdf770aa5233e19ff7e4c909
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-29 09:28:21 +0000

    29 Jun 2023, 14:58

---
+++ src/domain/use-cases/machine/UnlockMachine.ts
@@ -0,0 +1,6 @@
+export default class UnlockMachine implements UnlockMachineUseCase{
+  execute(machineNumber: string, reservationId: string): void {
+    throw new Error("Method not implemented.");
+  }
+  
+}
