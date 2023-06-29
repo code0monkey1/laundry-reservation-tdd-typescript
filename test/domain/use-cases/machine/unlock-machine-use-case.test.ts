7e3cb17972241f56ab568fefa62d8967513d6b51
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-29 09:33:02 +0000

    29 Jun 2023, 15:03

--- test/domain/use-cases/machine/unlock-machine-use-case.test.ts
+++ test/domain/use-cases/machine/unlock-machine-use-case.test.ts
@@ -1,8 +1,25 @@
+import UnlockMachine from "../../../../src/domain/use-cases/machine/UnlockMachine"
+import { getMachineNumber, getReservationId } from "../../../../src/domain/utils"
+
 describe('Unlock Machine Use Case', () => {
       
     test('unlocks machine given the machine number and reservation id',()=>{
+     
+          //Arrange
+          const unlockMachine = new UnlockMachine()
+          const machineNumber = getMachineNumber()
+          const reservationId = getReservationId()
+          
+          jest.spyOn(unlockMachine,'execute').mockImplementation(()=>{
 
+                Promise.resolve(true)
+          })
 
-      
+          //Act
+          unlockMachine.execute(machineNumber,reservationId)
+          
+
+          //Assert
+
     })
 })
